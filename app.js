ed74a6f0d8ac04751c5dd759117fc4aad5a1277f
Diwakar <diwakar.singh313@gmail.com>
2021-11-06 02:40:38 +0530

    minor session and cookie update

--- app.js
+++ app.js
@@ -40,11 +40,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize())
 
 const sessionConfig = {
+  name: 'session',
   secret: 'This is a secret!',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
